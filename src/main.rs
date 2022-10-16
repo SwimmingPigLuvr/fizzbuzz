cc72938f038fc9c97381553fbf389212edb3d26e
SwimmingPigLuvr <dnevell95@gmail.com>
2022-10-16 10:03:12 -0700

    🥤🐝✅ nice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+// use std::{time::Duration, thread};
 use owo_colors::OwoColorize;
 fn fizzbuzz() {
     for f in 1..100 {
@@ -12,20 +14,34 @@ fn fizzbuzz() {
     }
 }
 
-fn soda_bee() {
-    for f in 1..100 {
-        match (f % 3, f % 5) {
-            (0, 0) => {println!("🥤🐝")}
-            (0, _) => {println!("🥤")}
-            (_, 0) => {println!("🐝")}
-            _ => {
-                println!("{}", f.cyan().dimmed())
-            }
-        }
+fn soda_bee(upto: i32) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for f in 1..=upto {
+        result.push(match (f % 3, f % 5, f) {
+            (0, 0, _) => "🥤🐝".into(),
+            (_, _, 69) => "nice".to_string(),
+            (0, _, _) => "🥤".into(),
+            (_, 0, _) => "🐝".into(),
+            (_, _, _) => f.to_string()
+        })
     }
+    result
 }
 
 fn main() {
     // fizzbuzz()
-    soda_bee()
+
+    println!("{}", ("Starting FIZZBUZZ app").yellow().bold());
+    println!("{}", ("please input a number :").yellow().dimmed());
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("can't read");
+    let upto = input.trim().parse().unwrap();
+
+   
+
+    let vector = soda_bee(upto);
+    for j in vector.into_iter(){
+                println!("{}", j.bright_green());
+    }
+
 }
